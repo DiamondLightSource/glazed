701cb025fd22e79e95ecfb8e3a56fb483bdd3bca
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-09-18 11:22:20 +0100

    Remove unwraps from tiled_client

--- src/clients/tiled_client.rs
+++ src/clients/tiled_client.rs
@@ -5,14 +5,14 @@ pub struct TiledClient;
 impl TiledClient {
     async fn request(&self) -> reqwest::Response {
         println!("Requesting data from tiled");
-        reqwest::get("http://127.0.0.1:8000/api/v1/").await.unwrap()
+        reqwest::get("http://127.0.0.1:8000/api/v1/").await.expect(&format!("Request failed"))
     }
 }
 impl Client for TiledClient {
     async fn metadata(&self) -> Metadata {
-        let metadata_json = self.request().await.json().await.unwrap();
+        let metadata_json = self.request().await.json().await.unwrap_or(serde_json::json!(""));
 
-        let md: Metadata = serde_json::from_value(metadata_json).unwrap();
+        let md: Metadata = serde_json::from_value(metadata_json).expect("Could not construct metadata struct");
 
         md
     }
