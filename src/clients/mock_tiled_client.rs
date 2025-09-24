b5c3b10a8d42d83096ecc27c3241fbce6f06420c
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-09-24 11:11:08 +0100

    Move src/resources/tiled_metadata.json -> resources/tiled_metadata.json

--- src/clients/mock_tiled_client.rs
+++ src/clients/mock_tiled_client.rs
@@ -9,7 +9,7 @@ impl Client for MockTiledClient {
     async fn metadata(&self) -> ClientResult<Metadata> {
         println!("Requesting data from mock");
 
-        let path = Path::new("./src/resources/tiled_metadata.json");
+        let path = Path::new("./resources/tiled_metadata.json");
         let file = File::open(&path)?;
 
         Ok(serde_json::from_reader(file)?)
