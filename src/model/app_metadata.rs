f7ce8143474d1ad8c60b726c31fe51eb211e300f
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-10-30 16:51:40 +0000

    Rename metadata to appMetadata

--- src/model/app_metadata.rs
+++ src/model/app_metadata.rs
@@ -4,7 +4,7 @@ use async_graphql::SimpleObject;
 use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, SimpleObject)]
-pub struct Metadata {
+pub struct AppMetadata {
     pub api_version: i64,
     pub library_version: String,
     pub queries: Vec<String>,
