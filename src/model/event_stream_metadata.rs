8627551a38d59af54ce6683d7cf7d8ab5ef9890d
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-11-04 18:19:51 +0000

    Add event stream md

--- src/model/event_stream_metadata.rs
+++ src/model/event_stream_metadata.rs
@@ -1,9 +1,15 @@
+use std::collections::HashMap;
+
 use async_graphql::SimpleObject;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use uuid::Uuid;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, SimpleObject)]
 pub struct EventStreamMetadata {
-    configuration: Value,
-    data_keys: Value,
+    configuration: HashMap<String, HashMap<String, Value>>,
+    data_keys: HashMap<String, HashMap<String, Value>>,
+    time: i64,
+    uid: Uuid,
+    hints: HashMap<String, Value>,
 }
