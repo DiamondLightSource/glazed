9671ae6791a94cbad63c366fe70e39a83451004c
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-11-10 18:20:16 +0000

    Refactor model

--- src/model/node.rs
+++ src/model/node.rs
@@ -1,12 +1,16 @@
 use async_graphql::SimpleObject;
 use serde::{Deserialize, Serialize};
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, SimpleObject)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, SimpleObject)]
 pub struct Links {
     #[serde(rename = "self")]
     #[graphql(name = "self")]
     pub self_field: String,
     pub documentation: Option<String>,
+    pub first: Option<String>,
+    pub last: Option<String>,
+    pub next: Option<String>,
+    pub prev: Option<String>,
     pub search: Option<String>,
     pub full: Option<String>,
 }
