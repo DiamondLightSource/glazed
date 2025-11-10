f86409baf8ef10f3207362d0d1fdb83b24076e66
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-11-10 17:38:31 +0000

    Use node links in app md

--- src/model/app.rs
+++ src/model/app.rs
@@ -2,19 +2,13 @@ use async_graphql::SimpleObject;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
+use crate::model::node;
+
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, SimpleObject)]
 pub struct AppMetadata {
     pub api_version: i64,
     pub library_version: String,
     pub queries: Vec<String>,
-    pub links: AppMetadataLinks,
+    pub links: node::Links,
     pub meta: Value,
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, SimpleObject)]
-pub struct AppMetadataLinks {
-    #[serde(rename = "self")]
-    #[graphql(name = "self")]
-    pub self_field: String,
-    pub documentation: Option<String>,
 }
