5969f57758f7e4a0d95a220c3cb06886fb43e689
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-11-11 11:01:07 +0000

    Move client tests from model to client

--- src/clients.rs
+++ src/clients.rs
@@ -66,3 +66,82 @@ impl std::fmt::Display for ClientError {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use async_graphql::{EmptyMutation, EmptySubscription, Schema, Value};
+    use httpmock::MockServer;
+    use url::Url;
+
+    use crate::TiledQuery;
+    use crate::clients::TiledClient;
+
+    fn build_schema(url: &str) -> Schema<TiledQuery, EmptyMutation, EmptySubscription> {
+        Schema::build(
+            TiledQuery(TiledClient {
+                address: Url::parse(url).unwrap(),
+            }),
+            EmptyMutation,
+            EmptySubscription,
+        )
+        .finish()
+    }
+
+    #[tokio::test]
+    async fn test_server_unavailable() {
+        let schema = build_schema("http://tiled.example.com");
+        let response = schema.execute("{appMetadata { apiVersion } }").await;
+
+        assert_eq!(response.data, Value::Null);
+        assert_eq!(
+            response.errors[0].message,
+            "Tiled server error: error sending request for url (http://tiled.example.com/api/v1/)"
+        );
+        assert_eq!(response.errors.len(), 1);
+    }
+
+    #[tokio::test]
+    async fn test_internal_tiled_error() {
+        let server = MockServer::start();
+        let mock = server
+            .mock_async(|when, then| {
+                when.method("GET").path("/api/v1/");
+                then.status(503);
+            })
+            .await;
+        let schema = build_schema(&server.base_url());
+        let response = schema.execute("{appMetadata { apiVersion } }").await;
+        let actual = &response.errors[0].message;
+        let expected =
+            "Tiled server error: HTTP status server error (503 Service Unavailable) for url";
+
+        assert_eq!(response.data, Value::Null);
+        assert!(
+            actual.starts_with(expected),
+            "Unexpected error: {actual} \nExpected: {expected} [...]"
+        );
+        assert_eq!(response.errors.len(), 1);
+        mock.assert();
+    }
+
+    #[tokio::test]
+    async fn test_invalid_server_response() {
+        let server = MockServer::start();
+        let mock = server
+            .mock_async(|when, then| {
+                when.method("GET").path("/api/v1/");
+                then.status(200).body("{}");
+            })
+            .await;
+        let schema = build_schema(&server.base_url());
+        let response = schema.execute("{appMetadata { apiVersion } }").await;
+
+        assert_eq!(response.data, Value::Null);
+        assert_eq!(response.errors.len(), 1);
+        assert_eq!(
+            response.errors[0].message,
+            "Invalid response: missing field `api_version` at line 1 column 2, response: {}"
+        );
+        mock.assert();
+    }
+}
