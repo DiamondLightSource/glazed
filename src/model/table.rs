0c2fa18543d89c79665363dd353675613018adbb
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-11-27 12:34:20 +0000

    Remove table type alias

--- src/model/table.rs
+++ src/model/table.rs
@@ -1,10 +1,6 @@
-use std::collections::HashMap;
-
 use async_graphql::SimpleObject;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-
-pub type Table = HashMap<String, Vec<Value>>;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, SimpleObject)]
 pub struct TableStructure {
