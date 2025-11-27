7b13e53875571ce218e4f0a969a70611370c9754
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-11-27 12:58:08 +0000

    Underscore variable

--- src/model.rs
+++ src/model.rs
@@ -20,7 +20,7 @@ impl TiledQuery {
         Ok(ctx.data::<TiledClient>()?.app_metadata().await?)
     }
 
-    async fn instrument_session(&self, ctx: &Context<'_>, name: String) -> InstrumentSession {
+    async fn instrument_session(&self, _ctx: &Context<'_>, name: String) -> InstrumentSession {
         InstrumentSession { name }
     }
 }
